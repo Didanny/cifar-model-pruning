acafe0631b4b0cdcf7fb80649e14c834c6043615
Didanny <daa50@mail.aub.edu>
2023-07-03 14:39:50 -0700

    Add script to download datasets

---
+++ dl_data.sh
@@ -0,0 +1,8 @@
+mkdir data
+cd data 
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+wget https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz
+tar -xzf cifar-10-python.tar.gz
+tar -xzf cifar-100-python.tar.gz
+rm cifar-10-python.tar.gz
+rm cifar-100-python.tar.gz
